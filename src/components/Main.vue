321e1aa437aef19df7743f0e2e616cbff5bd41d0
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-25 18:14:10 +0100

    ass style for card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,24 @@
 <template>
   <section class="main">
     <div class="container">
-      <div class="row">
-        <div class="col-6 col-md-4 col-lg-3">cd prova</div>
-      </div>
+      <ul class="container-cd-list">
+        <li>
+          <div class="card-cd">
+            <div class="cover-cd">
+              <img
+                src="https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg"
+                alt="titolo cd"
+              />
+            </div>
+            <div class="info-cd text-center mt-3">
+              <div class="title-cd mb-2 text-uppercase">Bad</div>
+              <div class="author mb-1">Michael Jackson</div>
+              <div class="year mb-1">1987</div>
+              <div class="genre mb-1">Pop</div>
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
@@ -20,6 +35,11 @@ export default {
   },
   created() {
     this.getCdList();
+  },
+  computed: {
+    dataOk() {
+      return this.cdList.length === 10 ? true : false;
+    },
   },
   methods: {
     getCdList() {
