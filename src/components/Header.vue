71d5d27f0a017cce69eec3425f5cd5eb388c8e3c
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-25 16:43:28 +0100

    add bootstrap, axios and compoinent for header

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header p-2">
+    <img src="../assets/logo.png" alt="logo" class="logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/header";
+</style>
