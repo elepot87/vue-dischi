055552f85c70973fb0c202b2a7131de551c7076a
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-25 17:16:46 +0100

    add function to get axios

---
+++ src/components/Main.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="main">
+    <div class="container">
+      <div class="row">
+        <div class="col-6 col-md-4 col-lg-3">cd prova</div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Main",
+  data() {
+    return {
+      cdList: null,
+    };
+  },
+  created() {
+    this.getCdList;
+  },
+  methods: {
+    getCdList() {
+      // Get cd list from API
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((result) => {
+          console.log(result.data);
+          this.cdList = result.data;
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/main";
+</style>
